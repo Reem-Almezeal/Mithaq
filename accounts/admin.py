9fa5cf2c65fb1e2748365e2f6ea2e98ce1d2d049
Rimas Alshahrani <remasmushbab@gmail.com>
2026-05-11 19:02:29 +0300

    feat(accounts): - custom User model with email login - sign_up, sign_in, log_out views - accounts urls - signin/signup templates with responsive CSS - custom admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'national_id', 'mobile', 'is_staff']
+    search_fields = ['email', 'first_name', 'last_name', 'national_id']
+    ordering = ['email']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'national_id', 'mobile', 'date_of_birth')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'national_id', 'mobile', 'password1', 'password2'),
+        }),
+    )
