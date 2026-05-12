3d842980016962b796968d49fe01c4cd3053d860
Rimas Alshahrani <remasmushbab@gmail.com>
2026-05-12 10:43:07 +0300

    Greate profile page

--- accounts/models.py
+++ accounts/models.py
@@ -38,6 +38,9 @@ class User(AbstractUser):
     national_id = models.CharField(max_length=20, unique=True)
     mobile = models.CharField(max_length=15)
     date_of_birth = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
