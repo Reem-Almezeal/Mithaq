50c4d60c4bb0389efd1dcb2849eb53d233faa0bb
ghadi <ghadizarb@gmail.com>
2026-05-12 19:30:35 +0300

    add PaymentRecord model with Status choices and admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PaymentRecord
+
+
+@admin.register(PaymentRecord)
+class PaymentRecordAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'amount', 'currency', 'status', 'payment_method', 'created_at')
+    list_filter = ('status', 'payment_method', 'currency')
+    search_fields = ('user__email', 'moyasar_payment_id')
+    raw_id_fields = ('user',)
+    readonly_fields = ('moyasar_payment_id', 'created_at', 'updated_at')
