36f0bf49b675c3d24b4b2a8fdf8eb5fc43d215e7
Reem Almezeal <Reemk7832@gmail.com>
2026-06-20 02:55:48 +0300

    Configure production database

--- Mithaq/settings.py
+++ Mithaq/settings.py
@@ -114,12 +114,21 @@ WSGI_APPLICATION = 'Mithaq.wsgi.application'
 
 
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-    )
-}
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 
